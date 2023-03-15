d14bc0adc1f585c0a537347784dc6216ca301211
kapkande <kap.kande@gmail.com>
2023-03-15 23:40:25 +0300

    init: add express

--- src/index.js
+++ src/index.js
@@ -1,24 +1,33 @@
 // const http = require('http');
-const url = require('url');
+// const url = require('url');
 
-import './assets/styles/index.css';
-import satMainPage from './assets/components/mainPage/satMainPage'
-import satDonatePage from './assets/components/donatePage/setDonatePage';
+// import './assets/styles/index.css';
+// import satMainPage from './assets/components/mainPage/satMainPage'
+// import satDonatePage from './assets/components/donatePage/setDonatePage';
 // satDonatePage()
 
-http.createServer(function(req:any, res:any){
-    console.log(req);
-    let urlParts = url.parse(req.url);
-    if (req.method == 'get'){
-        switch (urlParts.pathnae){
-            case "/" || "":
-                satMainPage();
-                break;
-        }
-    }
-}).listen(3000);
+
 
 // function satMainPage () {
 //     const main = document.querySelector('.main')
 //     main.innerHTML='<a class="header__nav__item link" href="">Zoos</a>'
 // } 
+const path = require('path');
+const express = require('express');
+const app = express();
+
+
+const PORT = 2000;
+
+app.set('views', path.resolve(__dirname, 'ejs'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('index', { data: 'dasd' });
+})
+app.get('/donate/index.html', (req, res) => {
+    res.render('index', { data: 'dasd' });
+})
+app.listen(PORT, () => {
+    console.log('server has been started ' + PORT);
+})
