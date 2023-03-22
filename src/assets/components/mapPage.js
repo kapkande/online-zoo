d2193747283f9dc518cd41da93080034fa0ff8c2
kapkande <kap.kande@gmail.com>
2023-03-22 16:13:21 +0300

    init: add map

--- src/assets/components/mapPage.js
+++ src/assets/components/mapPage.js
@@ -1,34 +1,54 @@
 const svg = document.querySelector('.svg');
 const blockButtons = document.querySelector('.buttons');
+const map = document.querySelector('.map');
+const viewBox = svg.viewBox.animVal;
+const body = document.querySelector('.body');
+
+svg.setAttribute('width', map.clientWidth)
 blockButtons.addEventListener('click', (e) => {
-    const svg = document.querySelector('.svg');
     const target = e.target;
-    const s = svg.viewBox.animVal
     if (target.classList.contains('plus')) {
-        console.log(svg.viewBox.animVal);
-        svg.setAttribute('viewBox', `${s.x} ${s.y} ${s.width - 50} ${s.height - 50}`)
+        scaling(100);
     }
     if (target.classList.contains('minus')) {
-        svg.setAttribute('viewBox', `${s.x} ${s.y} ${s.width + 50} ${s.height + 50}`)
+        scaling(-100);
     }
 });
 
-
-svg.onmousedown = function () {
+svg.onmousedown = function (e) {
+    const viewBox = svg.attributes.viewBox.value.split(' ');
     svg.classList.add('map-active')
+    const x = e.pageX;
+    const y = e.pageY;
     document.onmousemove = function (e) {
-
         if (svg.classList.contains('map-active')) {
-            const svg = document.querySelector('.svg');
-            const s = svg.attributes.viewBox.value;
-            console.dir(s);
-            console.log(s.x , s.y);
-            svg.setAttribute('viewBox', `${e.pageX - s.x} ${e.pageY - s.y} ${s.width} ${s.height}`)
+            body.style.cursor = 'move';
+            svg.setAttribute('viewBox', `${x - e.pageX + +viewBox[0]} ${y - e.pageY + +viewBox[1]} ${viewBox[2]} ${viewBox[3]}`)
         }
-
     }
-    document.querySelector('.wrapper').onmouseup = function () {
-        // console.log(11);
-        svg.classList.remove('map-active')
+    document.querySelector('.wrapper').onmouseup = function (e) {
+        svg.classList.remove('map-active');
+        body.style.cursor = 'default';
     }
 }
+
+window.addEventListener('wheel', (e) => {
+    const target = e.target
+    if (target.closest('path')) {
+        scaling(e.deltaY > 0 ? -100 : 100);
+        body.classList.add('hidden');
+    } else {body.classList.remove('hidden'); }
+
+});
+
+function scaling(params) {
+    if (params > 0) {
+        if (150 > viewBox.width || 150 > viewBox.height) { return };
+        svg.setAttribute('viewBox', `${viewBox.x} ${viewBox.y} ${viewBox.width - 100} ${ viewBox.height - 100}`)
+    }
+    if (5000 < viewBox.width || 2000 < viewBox.height) { return };
+    if (params < 0) {
+        svg.setAttribute('viewBox', `${viewBox.x} ${viewBox.y} ${viewBox.width + 100} ${viewBox.height + 100}`)
+    }
+}
+
