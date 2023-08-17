4cca18002de8ca8b1dcf3988370a6ea9899e4268
kapkande <kap.kande@gmail.com>
2023-08-17 16:21:21 +0300

    init: add choice block in video section

--- online-zoo/pages/zoos/checkQuery.js
+++ online-zoo/pages/zoos/checkQuery.js
@@ -7,11 +7,9 @@ function checkQuery() {
     const title = document.querySelector('.video__title');
     title.textContent = targetDate.title;
 
-    // if(document.querySelector('.info__textBlock')){return};
+
     createTextBlock(targetDate)
-
-
-
+    createVideo(targetDate)
 
 }
 checkQuery()
@@ -19,10 +17,11 @@ checkQuery()
 function createTextBlock(targetDate) {
     const info = document.querySelector('.info');
     document.querySelector('.info__textBlock')?.remove();
+
     const textBlock = document.createElement('div');
     textBlock.classList.add("info__textBlock");
-   
-        targetDate.text.forEach(element => {
+
+    targetDate.text.forEach(element => {
         const item = document.createElement('div');
         item.classList.add('info__textBlock__item')
 
@@ -31,10 +30,7 @@ function createTextBlock(targetDate) {
         subtitle.textContent = element[0];
         item.appendChild(subtitle);
 
-        createDivElement(element.splice(1), item, subtitle)
-
-
-
+        createDivElement(element.slice(1), item, subtitle)
 
         textBlock.appendChild(item);
         info.appendChild(textBlock)
@@ -43,9 +39,8 @@ function createTextBlock(targetDate) {
 
 }
 
-function createDivElement(arr, item, subtitle) {
-    arr.forEach((e, i) => {
-        console.log(e);
+function createDivElement(arr, item) {
+    arr.forEach(e => {
         const text = document.createElement('div');
         text.classList.add('info__text');
         text.innerHTML = e;
@@ -53,3 +48,67 @@ function createDivElement(arr, item, subtitle) {
     });
 
 }
+
+function createVideo(targetDate) {
+    console.log(targetDate);
+    const video = document.querySelector('.video__view');
+    video.src = targetDate.video[0];
+
+    const choiceVideoBlock = document.querySelector('.video__choiceVideoBlock');
+
+    targetDate.video.forEach( (e, i) => {
+        const item = document.createElement('div');
+        item.classList.add('video__choice__item');
+        item.style.backgroundImage = `url(../../assets/imeges/zoos/slider/garillaForSlider-${i}.jpg)`
+        choiceVideoBlock.appendChild(item);
+       
+    });
+
+
+
+    
+}
+
+async function name() {
+    //     let response = await fetch(`https://img.youtube.com/vi/4bxwB4jD_Eg/mqdefault.jpg`);
+    // console.log(response);
+    //     let blob = await response.blob(); // скачиваем как Blob-объект
+
+    //     // создаём <img>
+    //     let img = document.createElement('img');
+    //     img.style = 'position:fixed;top:10px;left:10px;width:100px';
+    //     document.body.append(img);
+
+    //     // выводим на экран
+    //     img.src = URL.createObjectURL(blob);
+
+
+
+    // await Promise.all(targetDate.video.map(async (e) => {
+    //     const item = document.createElement('div');
+    //     item.classList.add('videochoiceitem');
+    //     const img = await fetch(`https://img.youtube.com/vi/4bxwB4jD_Eg/mqdefault.jpg`, {mode: 'no-cors'});
+    //     let blob = await img.blob();
+
+    //     choiceVideoBlock.appendChild(item);
+
+    //     console.log(img);
+    //     item.style.backgroundImage = `url(${blob})`
+    //     item.style.backgroundImage = URL.createObjectURL(blob);
+
+    //   }));
+
+
+
+   
+    await fetch('https://static-basket-01.wb.ru/vol1/crm-bnrs/bners1/big_spp_1708sng.jpg')
+    .then(response => response.blob())
+    .then(blob => {
+      const img = document.createElement('img');
+      img.style = 'position:fixed;top:10px;left:10px;width:100px';
+      img.src = URL.createObjectURL(blob);
+      document.body.appendChild(img);
+    });
+   
+}
+name()
